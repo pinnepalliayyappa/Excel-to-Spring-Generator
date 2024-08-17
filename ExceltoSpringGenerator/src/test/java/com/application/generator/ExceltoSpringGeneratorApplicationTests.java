f359367f86df0462dcc3ac7728a676852f8f1ae9
Ayyappa <pinnepalliayyappa@gmail.com>
2024-08-17 16:40:07 +0530

    Initial Commit

---
+++ ExceltoSpringGenerator/src/test/java/com/application/generator/ExceltoSpringGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceltoSpringGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
