bc1fa36c38af49e5f27973b8b13a2f5ff326e638
Phani Kumar <phanitalapaneni15@gmail.com>
2024-08-28 21:42:56 +0530

    created api to fetch the dependencies if present or hardcoded

--- ExceltoSpringGenerator/src/main/java/com/application/generator/controller/EntityGeneratorController.java
+++ ExceltoSpringGenerator/src/main/java/com/application/generator/controller/EntityGeneratorController.java
@@ -2,9 +2,11 @@ package com.application.generator.controller;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.application.generator.BaseFiles.SpringInitializer;
 import com.application.generator.BaseFiles.TemplateGenerator;
 import com.application.generator.dto.ClassesRequest;
 import com.application.generator.dto.TemplateRequest;
@@ -25,6 +28,9 @@ public class EntityGeneratorController {
 	TemplateGenerator templateGenerator;
     @Autowired
     FileReadService fileReadservice;
+    
+    @Autowired
+    SpringInitializer metadataService;
     
     @PostMapping("/project")
     public ResponseEntity<String> generateFile(@RequestBody TemplateRequest request) {
@@ -42,6 +48,11 @@ public class EntityGeneratorController {
     public List<ClassesRequest> readExcel(@RequestParam("file") MultipartFile file) throws IOException{	
     	List<ClassesRequest> result = fileReadservice.excelRead(file);
     	return result;
+    }
+    
+    @GetMapping("/dependencies")
+    public Map<String, Object> getDependencies() {
+        return metadataService.fetchInitializrMetadata();
     }
 
 }
