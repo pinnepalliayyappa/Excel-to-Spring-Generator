262a84b39ad8c569a75099e5a9c933d4c3d6da48
Phani Kumar <phanitalapaneni15@gmail.com>
2024-09-05 22:16:27 +0530

    Developing UI version 2

---
+++ Frontend/src/app/ui-v2/home-screen/home-screen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-screen',
+  templateUrl: './home-screen.component.html',
+  styleUrls: ['./home-screen.component.css']
+})
+export class HomeScreenComponent {
+
+}
---
+++ Frontend/src/app/ui-v2/ui-v2.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeScreenComponent } from './home-screen/home-screen.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeScreenComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UiV2Module { }
